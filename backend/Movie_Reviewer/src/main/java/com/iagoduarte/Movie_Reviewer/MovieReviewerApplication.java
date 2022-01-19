b3695c7897073f87da1fb3be119f15f602840be6
Iago Duarte <iagodsv@gmail.com>
2022-01-19 06:47:51 -0300

    primeiro commit do projeto

---
+++ backend/Movie_Reviewer/src/main/java/com/iagoduarte/Movie_Reviewer/MovieReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.iagoduarte.Movie_Reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieReviewerApplication.class, args);
+	}
+
+}
---
+++ backend/Movie_Reviewer/src/test/java/com/iagoduarte/Movie_Reviewer/MovieReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iagoduarte.Movie_Reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
